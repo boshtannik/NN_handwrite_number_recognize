0c43c5086a8bbb46c69ef8c6c7c922ed60d8a083
boshtannik <boshtannik@gmail.com>
2023-05-11 20:26:59 +0300

    forgotten file add

---
+++ install_data_small.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/makeyourownneuralnetwork/makeyourownneuralnetwork/master/mnist_dataset/mnist_train_100.csv
+wget https://raw.githubusercontent.com/makeyourownneuralnetwork/makeyourownneuralnetwork/master/mnist_dataset/mnist_test_10.csv
+
+mv ./mnist_train_100.csv ./mnist_train.csv
+mv ./mnist_test_10.csv ./mnist_test.csv
